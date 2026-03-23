d8ba00b22b07e4263b14cf99727c88e62a37456b
EA - Sabriny Kauany De Miranda Mendes Veloso <sabriny_veloso@educadventista.org>
2026-03-23 11:08:35 -0300

    Atualiza servidor e dependencias

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ app.listen(PORTA, () => {
   console.log(`Servidor rodando na porta ${PORTA}`);
 });
 
+app.get ("/", (req, res) => {
+  res.send("Bem-vindo à API de Tarefas!" );
+});
+
 app.get("/tarefas", (req, res) => {
   res.status(200).json(tarefas);
 });
@@ -31,4 +35,35 @@ app.post("/tarefas", (req, res) => {
   tarefas.push(novaTarefa);
 
   res.status(201).json(novaTarefa);
+});
+
+app.put("/tarefas/:id", (req, res) => {
+  const { id } = req.params;
+  const { titulo, concluida } = req.body;
+
+  const tarefa = tarefas.find(t => t.id == id);
+
+  if (!tarefa) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+
+  // Atualiza apenas se vier no body
+  if (titulo !== undefined) tarefa.titulo = titulo;
+  if (concluida !== undefined) tarefa.concluida = concluida;
+
+  res.status(200).json(tarefa);
+});
+
+app.delete("/tarefas/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = tarefas.findIndex(t => t.id == id);
+
+  if (index === -1) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+
+  tarefas.splice(index, 1);
+
+  res.status(200).json({ mensagem: "Tarefa removida com sucesso" });
 });
